409ff8bd65e36b926a4e65f7902615770fe1410c
Flake212 <111091522+Flake2121@users.noreply.github.com>
2024-05-17 23:10:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
   line(width,0,0,0)
   stroke(255);
   strokeWeight(1);
-  rect(0,0,100,10);
+  rect(0,0,100,20);
   stroke(0);
-  text("x: "+x+" y: "+y,3,15);
+  text("x: "+x+" y: "+y,5,15);
 }
