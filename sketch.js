77bc5c99fb0d3d93eddc6e115beb1c05b6dc2d80
Flake212 <111091522+Flake2121@users.noreply.github.com>
2024-05-17 22:14:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,37 @@
 var x = 0;
 var y = 0;
-var cx = 0.5
-var cy = 0.5
+var cx = 10;
+var cy = 10;
+var px=0;
+var py=0;
+
 
 function setup() {
   createCanvas(window.screen.width, window.screen.height);
   background(0);
   x = width / 2;
   y = height / 2;
-  frameRate(20);
+  px=x
+  py=y
+  frameRate(144);
+  strokeCap(PROJECT);
 }
 
 function draw() {
-  noStroke();
-  //background(220);
+  strokeWeight(30);
   colorMode(HSB);
-  fill((x/width)*255, ((y + 200)*height) / 2040, 255);
-  ellipse(x, y, 30, 30);
+  stroke((x/width)*255, ((y + 200)*height) / 2040, 255);
+  line(px,py,x,y);
   if (y + cy >= height - 15 || y + cy <= 15) {
     cy *= -1;
+    
   }
   if (x + cx >= width - 15 || x + cx <= 15) {
     cx *= -1;
   }
+  py=y;
+  px=x;
   x += cx;
   y += cy;
+  
 }
