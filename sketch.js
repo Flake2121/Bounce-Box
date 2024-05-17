75e18c528c985cbb0beca8956aa438f0eacf36dc
Flake212 <111091522+Flake2121@users.noreply.github.com>
2024-05-17 21:37:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function draw() {
   noStroke();
   //background(220);
   colorMode(HSB);
-  fill(x, (y + 200) / 8, 255);
+  fill((x/width)h*255, ((y + 200)*height) / 2040, 255);
   ellipse(x, y, 30, 30);
   if (y + cy >= height - 15 || y + cy <= 15) {
     cy *= -1;
